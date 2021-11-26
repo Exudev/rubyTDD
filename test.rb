f023d08333743c182dc6f88424dafcfbcec5dc69
PABLO GONZALEZ TAVAREZ <1094765@est.intec.edu.do>
2021-11-26 19:34:21 -0400

    second push

---
+++ test.rb
@@ -0,0 +1,16 @@
+require "./app"
+describe "temperature conversion functions" do
+  describe "#ftoc" do
+    it "converts freezing temperature" do
+      expect(ftoc(32)).to eq(0)
+    end
+  end
+    describe "temperature conversion functions" do
+      describe "#ctof" do
+        it "converts from celcius to fahrenheit tempeture" do
+          expect(ctof(25)).to eq(77)
+  end
+end
+end
+
+ 
